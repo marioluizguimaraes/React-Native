89c1051c50214712fe13c1ca535a04cbf5b0a273
mario <mariolsg.oficial@gmail.com>
2024-12-30 20:03:20 -0300

    .

---
+++ Primeiros-Passos/green-lantern/App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TextInput style={{backgroundColor:'black', borderRadius: 8, color:'white', padding: 12 }}>
+        Caixa de Texto:
+      </TextInput>
+      <Text style={style2.texto}>
+        Hello Word!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+const style2 = StyleSheet.create({
+    texto:{
+      fontSize: 24,
+      fontWeight: 'bold'
+    }
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
