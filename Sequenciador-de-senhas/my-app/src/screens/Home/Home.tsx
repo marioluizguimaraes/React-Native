7b349be5c990116e85820e47bcf657cf4c4c2384
mario <mariolsg.oficial@gmail.com>
2025-01-02 20:36:26 -0300

    .

--- Sequenciador-de-senhas/my-app/src/screens/Home/Home.tsx
+++ Sequenciador-de-senhas/my-app/src/screens/Home/Home.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import {Text, View } from 'react-native';
 import styles from './Style'
+import { Menu } from '../../components/Menu/Menu'
 
 export default function Home(){
     return(
         <View style={styles.container}>
-            <Text>Home Components!</Text>
-            <StatusBar style="auto" />
+            <Menu/>
         </View>
     )
 }
