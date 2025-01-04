4c9cc2e0a5a8ac4a5431cae60620fdfb73092a6a
mario <mariolsg.oficial@gmail.com>
2025-01-04 09:38:45 -0300

    .

---
+++ Sequenciador-de-senhas/my-app2/src/componets/ButtonImput/ButtonImput.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './ButtonImputStyles';
+
+export function ButtonImput() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
