5e50482315f1a3f97be993764f53b6257f3cf96e
mario <mariolsg.oficial@gmail.com>
2025-01-04 09:55:12 -0300

    .

--- Sequenciador-de-senhas/my-app2/src/componets/Logo/Logo.tsx
+++ Sequenciador-de-senhas/my-app2/src/componets/Logo/Logo.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
-
+import { Text, Image } from 'react-native';
 import { styles } from './LogoStyles';
-
+import logo from '../../../assets/file-lock.png'
 export function Logo() {
   return (
-    <View style={styles.container}>
-
-    </View>
+    <>
+      <Image source={logo} alt='cadiado roxo dentro de um reteangolo vertical branco'/>
+      <Text>Key Generator</Text>
+      <Text>Gere senhas seguras e manhetna-se seguro</Text>
+    </>
   );
 }
--- Sequenciador-de-senhas/my-app2/src/screens/Home/Home.tsx
+++ Sequenciador-de-senhas/my-app2/src/screens/Home/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import { styles } from './HomeStyles';
+import { Logo } from '../../componets/Logo/Logo';
 
 export function Home() {
   return (
     <View style={styles.container}>
-        
+      <Logo/>
     </View>
   );
 }
