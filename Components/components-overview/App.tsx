c742ea2d0a7300df06511f94cd022f8f8a1c07ed
mario <mariolsg.oficial@gmail.com>
2025-01-07 20:08:53 -0300

    .

---
+++ Components/components-overview/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>OLA MUNDO!</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
