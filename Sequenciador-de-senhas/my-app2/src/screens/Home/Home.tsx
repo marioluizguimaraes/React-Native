dc51962f4d1b92235f7ac14ebdb11e7d37be6988
mario <mariolsg.oficial@gmail.com>
2025-01-15 08:21:48 -0300

    .

--- Sequenciador-de-senhas/my-app2/src/screens/Home/Home.tsx
+++ Sequenciador-de-senhas/my-app2/src/screens/Home/Home.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
 import { styles } from './HomeStyles';
-import { Logo } from '../../componets/Logo/Logo';
+import { Logo } from '../../components/Logo/Logo';
 import { StatusBar } from 'expo-status-bar'
-import { ButtonImput } from '../../componets/ButtonInput/ButtonInput';
+import { ButtonImput } from '../../components/ButtonInput/ButtonInput';
 
 export function Home() {
   return (
